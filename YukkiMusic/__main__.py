c907cc09cd71fd8b27c63d3fbccb08cf7648f277
Asutosh Sahoo <79554993+iAsutoshS@users.noreply.github.com>
2025-05-29 20:47:47 +0530

    Update __main__.py
    Signed-off-by: Asutosh Sahoo <79554993+iAsutoshS@users.noreply.github.com>

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -97,7 +97,6 @@ async def init():
     await userbot.stop()
     await Yukki.stop()
 
-raise ValueError("This should be logged to file and console")
 
 def main():
     loop.run_until_complete(init())
@@ -106,3 +105,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+raise ValueError("This should be logged to file and console")
