a1a5e7a23e945288df47e14099277c02c3ddb401
Asutosh Sahoo <79554993+iAsutoshS@users.noreply.github.com>
2025-05-29 22:04:59 +0530

    Update youtube.py
    Signed-off-by: Asutosh Sahoo <79554993+iAsutoshS@users.noreply.github.com>

--- YukkiMusic/platforms/youtube.py
+++ YukkiMusic/platforms/youtube.py
@@ -150,7 +150,7 @@ class YouTube:
         cmd = [
             "yt-dlp",
             "--cookies-from-browser",
-            "brave",
+            "firefox",
             "-g",
             "-f",
             "best[height<=?720][width<=?1280]",
@@ -222,7 +222,7 @@ class YouTube:
             "noplaylist": True,
             "quiet": True,
             "extract_flat": "in_playlist",
-            "cookiesfrombrowser": ("brave",),
+            "cookiesfrombrowser": ("firefox",),
         }
         with YoutubeDL(options) as ydl:
             info_dict = ydl.extract_info(f"ytsearch: {q}", download=False)
@@ -250,7 +250,7 @@ class YouTube:
 
         ytdl_opts = {
             "quiet": True,
-            "cookiesfrombrowser": ("brave",),
+            "cookiesfrombrowser": ("firefox",),
         }
 
         ydl = YoutubeDL(ytdl_opts)
@@ -326,7 +326,7 @@ class YouTube:
                 "nocheckcertificate": True,
                 "quiet": True,
                 "no_warnings": True,
-                "cookiesfrombrowser": ("brave",),
+                "cookiesfrombrowser": ("firefox",),
                 "prefer_ffmpeg": True,
             }
 
@@ -349,7 +349,7 @@ class YouTube:
                 "quiet": True,
                 "no_warnings": True,
                 "prefer_ffmpeg": True,
-                "cookiesfrombrowser": ("brave",),
+                "cookiesfrombrowser": ("firefox",),
             }
 
             with YoutubeDL(ydl_optssx) as x:
@@ -372,7 +372,7 @@ class YouTube:
                 "no_warnings": True,
                 "prefer_ffmpeg": True,
                 "merge_output_format": "mp4",
-                "cookiesfrombrowser": ("brave",),
+                "cookiesfrombrowser": ("firefox",),
             }
 
             with YoutubeDL(ydl_optssx) as x:
@@ -399,7 +399,7 @@ class YouTube:
                         "preferredquality": "192",
                     }
                 ],
-                "cookiesfrombrowser": ("brave",),
+                "cookiesfrombrowser": ("firefox",),
             }
 
             with YoutubeDL(ydl_optssx) as x:
@@ -422,7 +422,7 @@ class YouTube:
                 command = [
                     "yt-dlp",
                     "--cookies-from-browser",
-                    "brave",
+                    "firefox",
                     "-g",
                     "-f",
                     "best",
